69d2b7ba0ab3cde04556d01199f1d2411152cc31
fowza <32482750+Mosekjaer@users.noreply.github.com>
2025-05-20 11:25:02 +0200

    Små motor rettelser

--- CarDriver/Motor/Motor.c
+++ CarDriver/Motor/Motor.c
@@ -10,32 +10,32 @@
 
 // Initialiserer styring af motoren 
 void Motor_Init() {
-	if (DDRE != 0xFF)
-		DDRE = 0xFF;
-		
-	if (PORTE != 0)
-		PORTE = 0;
-		
+	PORTB &= ~(1 << PB4); // Motor starter med fremad
+	DDRB |= (1 << PB4); // PB4 sættes til output
+	
+	// Sætter PE4 til 0 i PORTE og PE4 = 1 I DDRE
+	DDRE |= (1 << PE4); // PE4 gøres til output for PWM
+	
     // Nulstiller TCCR1A og TCCR1B først
     TCCR3A = 0;
     TCCR3B = 0;
 	
 	//Sætter TCCR3B og TCCR3A til 10 bit
 	//Har valgt timer 3, og den kører på ben OC3B (PE4).
-	TCCR3A |= (1 << WGM10);
-	TCCR3A |= (1 << WGM11);
+	TCCR3A |= (1 << WGM30);
+	TCCR3A |= (1 << WGM31);
 		
 	//Sætter prescaler til no prescaling
-	TCCR3B |= (1 << CS10);
+	TCCR3B |= (1 << CS30);
 		
-	//Kigger på slide 29 og kan se at det er nummer 3 man skal gå ud fra
-	TCCR3A |= (1 << COM1B1);
+	//Kigger på slide 29 og kan se at det er nummer 3 man skal gå ud fra (Ikke inverterings mode for OC3B)
+	TCCR3A |= (1 << COM3B1);
 }
 
 // Sætter hastigheden på motoren (0-100%)
 void Motor_SetSpeed(int speed) {
-	int motor_speed = 1023 * (speed / 100);
-	OCR1A = motor_speed; // duty cycle based on the motor speed provided.
+	int motor_speed = (1023 * speed) / 100;
+	OCR3B = motor_speed; // duty cycle based on the motor speed provided.
 }
 
 // Sætter motoren til at kører fremad 
